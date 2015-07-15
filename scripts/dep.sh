683741aa9f6055e70c0822bf2520d4b59ecaca4f
Tomohiko Himura <eiel.hal@gmail.com>
2015-07-15 20:04:26 +0900

    v0.0.4

--- scripts/dep.sh
+++ scripts/dep.sh
@@ -53,8 +53,7 @@ git \
 nodejs \
 mysql-client \
 postgresql-client \
-sqlite3 \
-gpg
+sqlite3
 
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -sSL https://get.rvm.io | bash -s stable
