e54606838f190c76aaed5ae659e8b53da8059f2f
Tomohiko Himura <eiel.hal@gmail.com>
2015-07-19 11:30:14 +0900

    2.2.2のrubyをインストール済みに

--- scripts/dep.sh
+++ scripts/dep.sh
@@ -59,6 +59,7 @@ gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB8
 curl -sSL https://get.rvm.io | bash -s stable
 source /etc/profile.d/rvm.sh
 rvm install 2.1.6
+rvm install 2.2.2
 gem install bundler
 
 # cleanup
